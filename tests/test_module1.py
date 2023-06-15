eedeebcda945b70dec7eace3bf3fd3e78beab892
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-06-15 17:02:13 +0200

    Test infraestructure working

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,12 +1,15 @@
 import unittest
 
-from examplepy.module1 import Number
+import sys
+sys.path.append("./src/pdf_signature_validator")
 
+from src.pdf_signature_validator import *
 
 class TestSimple(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual((Number(5) + Number(6)).value, 11)
+        # self.assertEqual((Number(5) + Number(6)).value, 11)
+        self.assertEqual(1 + 2, 3)
 
 
 if __name__ == '__main__':
