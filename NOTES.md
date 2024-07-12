53a0aedb523d601e085db037215ee926b7bb7a3b
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-07-12 12:33:45 +0200

    Validates some certificates

--- NOTES.md
+++ NOTES.md
@@ -5,3 +5,14 @@ FILE=<the sig0 file>
 openssl pkcs7 -in $FILE -inform DER -print_certs # To console
 openssl pkcs7 -in $FILE -inform DER -print_certs -out certs.pem # To certs.pem file
 ```
+
+Download ACCV certificates from:
+https://www.accv.es/en/servicios/administracion-publica/descarga-de-certificados-jerarquia/
+
+
+Convert binary .crt certificate to .pem format:
+
+```sh
+CERTIFICATE=<your .crt certificate>
+openssl x509 -inform DER -in "${CERTIFICATE}" -out "${CERTIFICATE%.crt}".pem
+```
--- foo.py
+++ foo.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 # Function to verify certificate against a CA certificate
@@ -11,16 +12,53 @@ def load_certs_from_file(pem_file):
         certs = f.read().split('\n\n')
     return certs
 
+def split_pem_chain(pem_file):
+    import re
+    import tempfile
+
+    # Split certificates based on the BEGIN/END CERTIFICATE delimiters
+    cert_pattern = re.compile(r'(-----BEGIN CERTIFICATE-----.+?-----END CERTIFICATE-----)', re.DOTALL)
+
+    # Read file and extract certificates
+    certificates = []
+    with open(pem_file, 'r') as file:
+        file_content = file.read()
+        certificates = re.findall(cert_pattern, file_content)
+
+    # List to store temporary file names
+    temp_files = []
+
+    # Process each certificate and save it to a temporary file
+    for cert in certificates:
+        # Create a temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(cert.strip().encode('utf-8'))  # Write certificate content to the temporary file
+            temp_files.append(temp_file.name)  # Store the temporary file name
+
+    return temp_files
+
 def extract_pem_file(filename, output_filename):
     result = subprocess.run(['openssl', 'pkcs7', '-in',  filename, '-inform', 'DER', '-print_certs', '-out', output_filename ], capture_output=True, text=True)
     return result.returncode == 0
 
+ROOT_CERTIFICATES_DIRECTORY='./tmp/certificates'
+def get_root_certificates():
+    directory = ROOT_CERTIFICATES_DIRECTORY
+    certificates = os.listdir(directory)
+
+    return[f"{directory}/{file}" for file in certificates if file.endswith('.pem')]
+
 # Main function
 def main():
     FILE='./tmp/test.pdf.sig0'
-    extract_pem_file(FILE, output_filename='./tmp/certs.pem')
-    certs = load_certs_from_file('certs.pem')
-    root_ca_files = ['root_ca_cert1.pem', 'root_ca_cert2.pem', 'root_ca_cert3.pem']  # List of your root CA certificates
+    EXTRACTED_PEM_FILE='./tmp/certs.pem'
+
+    root_ca_files = get_root_certificates()
+    extract_pem_file(FILE, output_filename=EXTRACTED_PEM_FILE)
+
+    certs = split_pem_chain(EXTRACTED_PEM_FILE)
+    # return
+    # certs = load_certs_from_file(EXTRACTED_PEM_FILE)
 
     # Verify each certificate in the chain against each root CA certificate
     for i in range(len(certs)):
